e13b4d821db437588766eae96050491413edcedb
Durdoon-gpt <131494180+Durdoon-gpt@users.noreply.github.com>
2023-05-14 20:55:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,24 @@
 from flask import Flask, render_template, Response
-import cv2
-import socket
+from picamera import PiCamera
+import time
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
 
 def generate_frames():
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 30
+    time.sleep(2)  # Temps d'attente pour permettre à la caméra de se stabiliser
+
     while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+        frame = camera.capture_bytes(format='jpeg')
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return render_template('index.html', host=socket.gethostbyname(socket.gethostname()))
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
